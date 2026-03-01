98427f849450821fdb394ee7680a5a12dfac5853
krapka211 <krapka211@gmail.com>
2026-03-01 20:00:51 +0100

    Update gh.js

--- gh.js
+++ gh.js
@@ -12,6 +12,7 @@
   FIX: Stagger animations on wood buttons (p01 sub-scenes) and kitchen cards (p02)
   FIX: All existing: SM watchdog, debounce, retry, consistency, walkIn pre-trans, sub-TL flush
 -->
+<script>
 (function(){
 'use strict';
 
@@ -1056,3 +1057,4 @@ document.readyState==='loading'
   : boot();
 
 })();
+</script>
